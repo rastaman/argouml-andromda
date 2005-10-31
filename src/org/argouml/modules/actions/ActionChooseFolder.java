de844d4fd9b9df6899c7d54885be8aa12313d83b
rastaman <rastaman@de070b14-b31d-0410-aece-a3330300c9b5>
2005-10-31 22:15:37 +0000

    Use only one file chooser.

--- src/org/argouml/modules/actions/ActionChooseFolder.java
+++ src/org/argouml/modules/actions/ActionChooseFolder.java
@@ -4,31 +4,35 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JFileChooser;
-import javax.swing.text.JTextComponent;
+import javax.swing.JTextField;
 
 import org.argouml.modules.container.ModuleContainer;
 
 public class ActionChooseFolder extends AbstractModuleAction {
     
     private String targetId;
-    
+
     private String label;
     
-    public ActionChooseFolder(ModuleContainer p, String target, String label) { 
+    private JFileChooser chooser;
+    
+    public ActionChooseFolder(ModuleContainer p, String target, String lbl) { 
         super(p); 
         targetId = target;
-        this.label = label;
+        label = lbl;
     }
-    
+
     public void actionPerformed(java.awt.event.ActionEvent e) {
-        JFileChooser chooser =  new JFileChooser();
-        chooser.setDialogTitle(parent.getSwingEngine().getLocalizer().getString(label));
-        chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        if (chooser==null) {
+            chooser =  new JFileChooser();
+            chooser.setDialogTitle(parent.localize(label));
+            chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);            
+        }
         int retval = chooser.showOpenDialog(parent.getParentFrame());
         if (retval == JFileChooser.APPROVE_OPTION) {
             File theFile = chooser.getSelectedFile();
             try {
-                JTextComponent target = (JTextComponent) parent.getSwingEngine().
+                JTextField target = (JTextField) parent.getSwingEngine().
                     find(targetId);
                 target.setText(theFile.getCanonicalPath());
             } catch (IOException e1) {
