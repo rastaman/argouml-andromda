57a5e34ff04397a23b4c1dabb5d1488f1a8c4092
rastaman <rastaman@de070b14-b31d-0410-aece-a3330300c9b5>
2005-12-11 11:44:29 +0000

    ComboxBox model and support class for SwixML.

---
+++ trunk/src/org/argouml/modules/gui/Item.java
@@ -0,0 +1,41 @@
+package org.argouml.modules.gui;
+
+public class Item {
+
+    private String name;
+    
+    private String value;
+    
+    public Item() {
+        super();
+    }
+
+    /**
+     * @return Returns the name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name The name to set.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return Returns the value.
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * @param value The value to set.
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
---
+++ trunk/src/org/argouml/modules/gui/ItemList.java
@@ -0,0 +1,11 @@
+package org.argouml.modules.gui;
+
+import java.util.ArrayList;
+
+public class ItemList extends ArrayList {
+
+    public ItemList() {
+        super();
+    }
+
+}
